ee5cdab0b1c4565083c33c55dce89547ba9a786e
perezah10 <profesionalap04.16@hotmail.com>
2024-01-27 02:02:10 -0400

    Ciclo for para números pares e impares

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " es par");
+    } else {
+        console.log(i + " es impar");
+    }
+}
