c89116c148e14600b7a31ba5963de735deeea814
perezah10 <profesionalap04.16@hotmail.com>
2024-01-27 03:09:38 -0400

    Proyecto 2.0

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function imprimir(){
     let btn = document.querySelector("#btnGo");
+    let lista = document.querySelector("#listResult");
     for (let i = 1; i <= 20; i++) {
+        let elementoLista = document.createElement("li");
         if (i % 2 == 0) {
-            console.log(i + " es par");
+            elementoLista.textContent = i + " es par";
         } else {
-            console.log(i + " es impar");
+            elementoLista.textContent = i + " es impar";
         }
+        lista.appendChild(elementoLista);
     }
     let title = document.querySelector("h1");
     title.style.color = "#5fa5f5";
